08ca595a7a6ae4e255adebf78b39a4b689af514c
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-03-04 23:05:51 +0000

    Experimental branch

---
+++ smplayer/experimental/clean_windows.bat
@@ -0,0 +1,13 @@
+cd getrev
+mingw32-make distclean
+cd ..
+
+cd zlib-1.2.6
+mingw32-make -fwin32\makefile.gcc clean
+
+cd ..\src
+mingw32-make distclean
+
+cd ..
+del src\translations\smplayer_*.qm
+
