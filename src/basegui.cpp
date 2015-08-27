f19af3d63a9bec698dddbf0bb1a69d5ab1c368d4
wilbert2000 <wplayer@xs4all.nl>
2015-08-27 10:11:23 +0200

    Removed TODO

--- src/basegui.cpp
+++ src/basegui.cpp
@@ -961,7 +961,7 @@ void BaseGui::createActions() {
 	connect( playPrevAct, SIGNAL(triggered()), playlist, SLOT(playPrev()) );
 
 	// Pan
-	//TODO: bindings for panning should probably be the inverse of these if you
+	// Bindings for panning should probably be the inverse of these if you
 	// ever want to rename move to pan.
 	moveUpAct = new MyAction(Qt::ALT | Qt::Key_Up, this, "move_up");
 	connect( moveUpAct, SIGNAL(triggered()), core, SLOT(panDown()) );
