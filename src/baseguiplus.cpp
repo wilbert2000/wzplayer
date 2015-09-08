24b803bb48883ff47cecd04ed5916819dc254c68
wilbert2000 <wplayer@xs4all.nl>
2015-09-08 23:38:32 +0200

    Reduced debug info

--- src/baseguiplus.cpp
+++ src/baseguiplus.cpp
@@ -458,9 +458,9 @@ void BaseGuiPlus::aboutToExitCompactMode() {
 #if DOCK_PLAYLIST
 void BaseGuiPlus::showPlaylist(bool b) {
 	qDebug("BaseGuiPlus::showPlaylist: %d", b);
-	qDebug("BaseGuiPlus::showPlaylist (before): playlist visible: %d", playlistdock->isVisible());
-	qDebug("BaseGuiPlus::showPlaylist (before): playlist position: %d, %d", playlistdock->pos().x(), playlistdock->pos().y());
-	qDebug("BaseGuiPlus::showPlaylist (before): playlist size: %d x %d", playlistdock->size().width(), playlistdock->size().height());
+	//qDebug("BaseGuiPlus::showPlaylist (before): playlist visible: %d", playlistdock->isVisible());
+	//qDebug("BaseGuiPlus::showPlaylist (before): playlist position: %d, %d", playlistdock->pos().x(), playlistdock->pos().y());
+	//qDebug("BaseGuiPlus::showPlaylist (before): playlist size: %d x %d", playlistdock->size().width(), playlistdock->size().height());
 
 	if ( !b ) {
 		playlistdock->hide();
@@ -478,10 +478,9 @@ void BaseGuiPlus::showPlaylist(bool b) {
 	}
 	//updateWidgets();
 
-	qDebug("BaseGuiPlus::showPlaylist (after): playlist visible: %d", playlistdock->isVisible());
-	qDebug("BaseGuiPlus::showPlaylist (after): playlist position: %d, %d", playlistdock->pos().x(), playlistdock->pos().y());
-	qDebug("BaseGuiPlus::showPlaylist (after): playlist size: %d x %d", playlistdock->size().width(), playlistdock->size().height());
-
+	//qDebug("BaseGuiPlus::showPlaylist (after): playlist visible: %d", playlistdock->isVisible());
+	//qDebug("BaseGuiPlus::showPlaylist (after): playlist position: %d, %d", playlistdock->pos().x(), playlistdock->pos().y());
+	//qDebug("BaseGuiPlus::showPlaylist (after): playlist size: %d x %d", playlistdock->size().width(), playlistdock->size().height());
 }
 
 void BaseGuiPlus::playlistClosed() {
