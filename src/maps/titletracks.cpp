323de9766f4fe1cc3e73e2fa5c48b363cf382f7f
wilbert2000 <wplayer@xs4all.nl>
2015-12-15 18:44:17 +0100

    Fix translation "Title" and "Track"

--- src/maps/titletracks.cpp
+++ src/maps/titletracks.cpp
@@ -38,9 +38,9 @@ QString TTitleData::getDisplayName(bool add_duration) const {
 
 	if (name.isEmpty()) {
 		if (isTrack) {
-			dname = qApp->translate("Maps::TTitleData", "Track %1").arg(dname);
+			dname = qApp->translate("Gui::TPlaylist", "Track %1").arg(dname);
 		} else {
-			dname = qApp->translate("Maps::TTitleData", "Title %1").arg(dname);
+			dname = qApp->translate("Gui::TPlaylist", "Title %1").arg(dname);
 		}
 	} else {
 		dname += " " + name;
