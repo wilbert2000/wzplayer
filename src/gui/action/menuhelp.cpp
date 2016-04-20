c70c2534c5b15bbad3a3cc030d237ae2b220b2cc
wilbert2000 <wplayer@xs4all.nl>
2016-04-20 12:02:08 +0200

    Use update icon from pref for "Check for updates"

--- src/gui/action/menuhelp.cpp
+++ src/gui/action/menuhelp.cpp
@@ -11,7 +11,7 @@ TMenuHelp::TMenuHelp(QWidget* parent)
 	TAction* a = new TAction(this, "cl_options", tr("&Command line options"), "cl_help");
 	connect(a, SIGNAL(triggered()), parent, SLOT(helpCLOptions()));
 
-	a = new TAction(this, "check_updates", tr("Check for &updates"));
+    a = new TAction(this, "check_updates", tr("Check for &updates"), "pref_updates");
 	connect(a, SIGNAL(triggered()), parent, SLOT(helpCheckUpdates()));
 
 	a = new TAction(this, "about", tr("About &WZPlayer"), "logo");
