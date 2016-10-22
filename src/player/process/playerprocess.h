7e1cdf2bbb7c267e3c6640fdeae7d4672a1eed6c
wilbert2000 <wplayer@xs4all.nl>
2016-10-22 17:13:22 +0200

    Formatting

--- src/player/process/playerprocess.h
+++ src/player/process/playerprocess.h
@@ -40,12 +40,12 @@ class TPlayerProcess : public TProcess {
     DECLARE_QCLASS_LOGGER
 
 public:
-	enum ScreenshotType { Single = 0, Multiple = 1 };
+    enum ScreenshotType { Single = 0, Multiple = 1 };
 
     static TPlayerProcess* createPlayerProcess(QObject* parent, TMediaData* md);
 
     explicit TPlayerProcess(QObject* parent, TMediaData* mdata);
-	virtual ~TPlayerProcess() {}
+    virtual ~TPlayerProcess() {}
 
     bool isRunning() const { return state() == QProcess::Running; }
     bool isReady() const {
@@ -54,141 +54,141 @@ public:
                && isRunning();
     }
 
-	virtual bool startPlayer();
+    virtual bool startPlayer();
 
     void writeToStdin(const QString& text, bool log = true);
 
-	// Command line options
+    // Command line options
     virtual void setMedia(const QString& media) = 0;
-	virtual void setFixedOptions() = 0;
-	virtual void disableInput() = 0;
+    virtual void setFixedOptions() = 0;
+    virtual void disableInput() = 0;
     virtual void setOption(const QString& option_name,
                            const QVariant& value = QVariant()) = 0;
-	virtual void addUserOption(const QString& option) = 0;
+    virtual void addUserOption(const QString& option) = 0;
     virtual void addVF(const QString& filter_name,
                        const QVariant& value = QVariant()) = 0;
     virtual void addAF(const QString& filter_name,
                        const QVariant& value = QVariant()) = 0;
-	virtual void addStereo3DFilter(const QString& in, const QString& out) = 0;
+    virtual void addStereo3DFilter(const QString& in, const QString& out) = 0;
     virtual void setSubStyles(const Settings::TAssStyles& styles,
                               const QString& assStylesFile = QString::null) = 0;
     virtual void setImageDuration(int duration);
 
-	// Slave commands
-	void quit(int exit_code);
-	virtual void setVolume(int v) = 0;
-	virtual void setOSDLevel(int level) = 0;
-	virtual void setAudio(int ID) = 0;
-	virtual void setVideo(int ID) = 0;
-	virtual void setSubtitle(SubData::Type type, int ID) = 0;
-	virtual void disableSubtitles() = 0;
-	virtual void setSecondarySubtitle(SubData::Type type, int ID) = 0;
-	virtual void disableSecondarySubtitles() = 0;
-	virtual void setSubtitlesVisibility(bool b) = 0;
+    // Slave commands
+    void quit(int exit_code);
+    virtual void setVolume(int v) = 0;
+    virtual void setOSDLevel(int level) = 0;
+    virtual void setAudio(int ID) = 0;
+    virtual void setVideo(int ID) = 0;
+    virtual void setSubtitle(SubData::Type type, int ID) = 0;
+    virtual void disableSubtitles() = 0;
+    virtual void setSecondarySubtitle(SubData::Type type, int ID) = 0;
+    virtual void disableSecondarySubtitles() = 0;
+    virtual void setSubtitlesVisibility(bool b) = 0;
     virtual void seekPlayerTime(double secs, int mode, bool precise,
                                 bool currently_paused) = 0;
     virtual void seek(double secs, int mode, bool precise,
                       bool currently_paused);
-	virtual void mute(bool b) = 0;
-	virtual void setPause(bool b) = 0;
-	virtual void frameStep() = 0;
-	virtual void frameBackStep() = 0;
-	virtual void showOSDText(const QString& text, int duration, int level) = 0;
-	virtual void showFilenameOnOSD() = 0;
-	virtual void showTimeOnOSD() = 0;
-	virtual void setContrast(int value) = 0;
-	virtual void setBrightness(int value) = 0;
-	virtual void setHue(int value) = 0;
-	virtual void setSaturation(int value) = 0;
-	virtual void setGamma(int value) = 0;
-	virtual void setChapter(int ID) = 0;
-	virtual void nextChapter(int delta) = 0;
-	virtual void setAngle(int ID) = 0;
-	virtual void nextAngle() = 0;
-	virtual void setExternalSubtitleFile(const QString& filename) = 0;
-	virtual void setSubPos(int pos) = 0;
-	virtual void setSubScale(double value) = 0;
-	virtual void setSubStep(int value) = 0;
-	virtual void seekSub(int value) = 0;
-	virtual void setSubForcedOnly(bool b) = 0;
-	virtual void setSpeed(double value) = 0;
-	virtual void enableKaraoke(bool b) = 0;
-	virtual void enableExtrastereo(bool b) = 0;
-	virtual void enableVolnorm(bool b, const QString& option) = 0;
-	virtual void setAudioEqualizer(const QString& values) = 0;
-	virtual void setAudioDelay(double delay) = 0;
-	virtual void setSubDelay(double delay) = 0;
-	virtual void setLoop(int v) = 0;
+    virtual void mute(bool b) = 0;
+    virtual void setPause(bool b) = 0;
+    virtual void frameStep() = 0;
+    virtual void frameBackStep() = 0;
+    virtual void showOSDText(const QString& text, int duration, int level) = 0;
+    virtual void showFilenameOnOSD() = 0;
+    virtual void showTimeOnOSD() = 0;
+    virtual void setContrast(int value) = 0;
+    virtual void setBrightness(int value) = 0;
+    virtual void setHue(int value) = 0;
+    virtual void setSaturation(int value) = 0;
+    virtual void setGamma(int value) = 0;
+    virtual void setChapter(int ID) = 0;
+    virtual void nextChapter(int delta) = 0;
+    virtual void setAngle(int ID) = 0;
+    virtual void nextAngle() = 0;
+    virtual void setExternalSubtitleFile(const QString& filename) = 0;
+    virtual void setSubPos(int pos) = 0;
+    virtual void setSubScale(double value) = 0;
+    virtual void setSubStep(int value) = 0;
+    virtual void seekSub(int value) = 0;
+    virtual void setSubForcedOnly(bool b) = 0;
+    virtual void setSpeed(double value) = 0;
+    virtual void enableKaraoke(bool b) = 0;
+    virtual void enableExtrastereo(bool b) = 0;
+    virtual void enableVolnorm(bool b, const QString& option) = 0;
+    virtual void setAudioEqualizer(const QString& values) = 0;
+    virtual void setAudioDelay(double delay) = 0;
+    virtual void setSubDelay(double delay) = 0;
+    virtual void setLoop(int v) = 0;
     virtual void takeScreenshot(ScreenshotType t,
                                 bool include_subtitles = false) = 0;
-	virtual void switchCapturing() = 0;
-	virtual void setTitle(int ID) = 0;
+    virtual void switchCapturing() = 0;
+    virtual void setTitle(int ID) = 0;
     virtual void changeVF(const QString& filter, bool enable,
                           const QVariant& option = QVariant()) = 0;
     virtual void changeStereo3DFilter(bool enable, const QString& in,
                                       const QString& out) = 0;
 
-	virtual void discSetMousePos(int x, int y) = 0;
-	virtual void discButtonPressed(const QString& button_name) = 0;
+    virtual void discSetMousePos(int x, int y) = 0;
+    virtual void discButtonPressed(const QString& button_name) = 0;
 
-	virtual void setAspect(double aspect) = 0;
+    virtual void setAspect(double aspect) = 0;
     virtual void setZoomAndPan(double zoom, double pan_x, double pan_y,
                                int osd_levl) = 0;
 
 #if PROGRAM_SWITCH
-	virtual void setTSProgram(int ID) = 0;
+    virtual void setTSProgram(int ID) = 0;
 #endif
 
-	virtual void toggleDeinterlace() = 0;
-	virtual void setOSDScale(double value) = 0;
-	virtual void setChannelsFile(const QString &) = 0;
+    virtual void toggleDeinterlace() = 0;
+    virtual void setOSDScale(double value) = 0;
+    virtual void setChannelsFile(const QString &) = 0;
 
-	void setScreenshotDirectory(const QString& dir) { screenshot_dir = dir; }
-	QString screenshotDirectory() const { return screenshot_dir; }
-	virtual void setCaptureDirectory(const QString& dir);
+    void setScreenshotDirectory(const QString& dir) { screenshot_dir = dir; }
+    QString screenshotDirectory() const { return screenshot_dir; }
+    virtual void setCaptureDirectory(const QString& dir);
 
-	// Save current state to restore it after a restart
-	virtual void save() = 0;
+    // Save current state to restore it after a restart
+    virtual void save() = 0;
 
 // Signals
 signals:
     void processFinished(bool normal_exit, int exit_code, bool eof);
 
-	void playerFullyLoaded();
+    void playerFullyLoaded();
 
-	void receivedVideoOut();
-	void durationChanged(double);
-	void receivedPosition(double sec);
-	void receivedPause();
+    void receivedVideoOut();
+    void durationChanged(double);
+    void receivedPosition(double sec);
+    void receivedPause();
 
-	void receivedMessage(const QString&);
-	void receivedBuffering();
-	void receivedBufferingEnded();
-	void receivedScreenshot(const QString&);
-	void receivedUpdatingFontCache();
+    void receivedMessage(const QString&);
+    void receivedBuffering();
+    void receivedBufferingEnded();
+    void receivedScreenshot(const QString&);
+    void receivedUpdatingFontCache();
 
-	void receivedStreamTitle();
+    void receivedStreamTitle();
 
-	//! Emitted if one or more video track(s) added or changed
-	void receivedVideoTracks();
-	//! Emitted if player changed video track
-	void receivedVideoTrackChanged(int);
+    //! Emitted if one or more video track(s) added or changed
+    void receivedVideoTracks();
+    //! Emitted if player changed video track
+    void receivedVideoTrackChanged(int);
 
-	//! Emitted if audio track(s) added or changed
-	void receivedAudioTracks();
-	//! Emitted if player changed audio track
-	void receivedAudioTrackChanged(int);
+    //! Emitted if audio track(s) added or changed
+    void receivedAudioTracks();
+    //! Emitted if player changed audio track
+    void receivedAudioTrackChanged(int);
 
-	//! Emitted if a new subtitle has been added or an old one changed
-	void receivedSubtitleTracks();
-	//! Emitted if player changed subtitle track
-	void receivedSubtitleTrackChanged();
+    //! Emitted if a new subtitle has been added or an old one changed
+    void receivedSubtitleTracks();
+    //! Emitted if player changed subtitle track
+    void receivedSubtitleTrackChanged();
 
-	void receivedTitleTracks();
-	void receivedTitleTrackChanged(int);
+    void receivedTitleTracks();
+    void receivedTitleTrackChanged(int);
 
-	void receivedChapters();
-	void receivedAngles();
+    void receivedChapters();
+    void receivedAngles();
 
     void videoBitRateChanged(int bitrate);
     void audioBitRateChanged(int bitrate);
@@ -196,46 +196,46 @@ signals:
 protected:
     TMediaData* md;
 
-	QString screenshot_dir;
-	QString capture_filename;
+    QString screenshot_dir;
+    QString capture_filename;
 
-	bool notified_player_is_running;
-	int waiting_for_answers;
+    bool notified_player_is_running;
+    int waiting_for_answers;
     bool paused;
 
-	bool received_end_of_file;
-	bool quit_send;
-	int exit_code_override;
+    bool received_end_of_file;
+    bool quit_send;
+    int exit_code_override;
 
     QString temp_file_name;
 
-	double guiTimeToPlayerTime(double sec);
-	double playerTimeToGuiTime(double sec);
+    double guiTimeToPlayerTime(double sec);
+    double playerTimeToGuiTime(double sec);
 
-	void notifyTitleTrackChanged(int new_title);
-	void notifyDuration(double duration);
-	virtual void checkTime(double sec);
+    void notifyTitleTrackChanged(int new_title);
+    void notifyDuration(double duration);
+    virtual void checkTime(double sec);
     void notifyTime(double time_sec);
-	bool waitForAnswers();
+    bool waitForAnswers();
 
-	virtual void playingStarted();
-	virtual bool parseLine(QString& line);
-	virtual bool parseAudioProperty(const QString& name, const QString& value);
-	virtual bool parseVideoProperty(const QString& name, const QString& value);
-	virtual bool parseMetaDataProperty(QString name, QString value);
-	virtual bool parseProperty(const QString& name, const QString& value);
+    virtual void playingStarted();
+    virtual bool parseLine(QString& line);
+    virtual bool parseAudioProperty(const QString& name, const QString& value);
+    virtual bool parseVideoProperty(const QString& name, const QString& value);
+    virtual bool parseMetaDataProperty(QString name, QString value);
+    virtual bool parseProperty(const QString& name, const QString& value);
 
 protected slots:
     virtual void onFinished(int exitCode, QProcess::ExitStatus exitStatus);
 
 private:
-	int line_count;
-	int waiting_for_answers_safe_guard;
+    int line_count;
+    int waiting_for_answers_safe_guard;
 
     QTemporaryFile temp_file;
 
-	bool parseAngle(const QString& value);
-	bool parseVO(const QString& vo, int sw, int sh, int dw, int dh);
+    bool parseAngle(const QString& value);
+    bool parseVO(const QString& vo, int sw, int sh, int dw, int dh);
 };
 
 } // namespace Process
