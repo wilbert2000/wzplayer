1f6e752b69b680597b72ff6720a030b2a85c530e
wilbert2000 <wplayer@xs4all.nl>
2016-11-16 15:13:08 +0100

    Formatting

--- src/gui/autohidetimer.cpp
+++ src/gui/autohidetimer.cpp
@@ -38,191 +38,191 @@ TAutoHideTimer::~TAutoHideTimer() {
 
 void TAutoHideTimer::start() {
 
-	autoHide = true;
-	QTimer::start();
+    autoHide = true;
+    QTimer::start();
 }
 
 void TAutoHideTimer::stop() {
 
-	autoHide = false;
-	QTimer::stop();
+    autoHide = false;
+    QTimer::stop();
 
-	// Show the widgets to save their visible state
-	setVisible(true);
+    // Show the widgets to save their visible state
+    setVisible(true);
 }
 
 void TAutoHideTimer::enable() {
 
-	enabled = true;
-	autoHideMouseLastPosition = QCursor::pos();
-	if (autoHide || autoHideMouse)
-		QTimer::start();
+    enabled = true;
+    autoHideMouseLastPosition = QCursor::pos();
+    if (autoHide || autoHideMouse)
+        QTimer::start();
 }
 
 void TAutoHideTimer::disable() {
 
-	enabled = false;
-	if (autoHide)
-		setVisible(true);
-	showHiddenMouse();
-	QTimer::stop();
+    enabled = false;
+    if (autoHide)
+        setVisible(true);
+    showHiddenMouse();
+    QTimer::stop();
 }
 
 void TAutoHideTimer::setAutoHideMouse(bool on) {
 
-	autoHideMouse = on;
-	autoHideMouseLastPosition = QCursor::pos();
-	if (autoHideMouse)
-		QTimer::start();
-	else
-		showHiddenMouse();
+    autoHideMouse = on;
+    autoHideMouseLastPosition = QCursor::pos();
+    if (autoHideMouse)
+        QTimer::start();
+    else
+        showHiddenMouse();
 }
 
 void TAutoHideTimer::add(QAction* action, QWidget* w) {
 
-	TAutoHideItem item(action, w);
-	items[action->objectName()] = item;
-	if (action->isChecked()) {
-		actions.append(action);
-		widgets.append(w);
-	}
-	connect(action, SIGNAL(toggled(bool)), this, SLOT(onActionToggled(bool)));
+    TAutoHideItem item(action, w);
+    items[action->objectName()] = item;
+    if (action->isChecked()) {
+        actions.append(action);
+        widgets.append(w);
+    }
+    connect(action, SIGNAL(toggled(bool)), this, SLOT(onActionToggled(bool)));
 }
 
 void TAutoHideTimer::setVisible(bool visible) {
 
-	settingVisible = true;
+    settingVisible = true;
 
-	for(int i = 0; i < actions.size(); i++) {
-		QAction* action = actions[i];
-		if (action->isChecked() != visible) {
-			action->trigger();
-		}
-	}
+    for(int i = 0; i < actions.size(); i++) {
+        QAction* action = actions[i];
+        if (action->isChecked() != visible) {
+            action->trigger();
+        }
+    }
 
-	settingVisible = false;
+    settingVisible = false;
 }
 
 bool TAutoHideTimer::visibleWidget() const {
 
-	for(int i = 0; i < widgets.size(); i++) {
-		if (widgets[i]->isVisible())
-			return true;
-	}
-	return false;
+    for(int i = 0; i < widgets.size(); i++) {
+        if (widgets[i]->isVisible())
+            return true;
+    }
+    return false;
 }
 
 bool TAutoHideTimer::hiddenWidget() const {
 
-	for(int i = 0; i < widgets.size(); i++) {
-		if (widgets[i]->isHidden())
-			return true;
-	}
-	return false;
+    for(int i = 0; i < widgets.size(); i++) {
+        if (widgets[i]->isHidden())
+            return true;
+    }
+    return false;
 }
 
 bool TAutoHideTimer::mouseInsideShowArea() const {
 
-	const int margin = 100;
+    const int margin = 100;
 
-	// Check bottom of screen
-	if (pref->fullscreen
-		&& QCursor::pos().y() > TDesktop::size(playerWindow).height() - margin) {
-		return true;
-	}
+    // Check bottom of screen
+    if (pref->fullscreen
+        && QCursor::pos().y() > TDesktop::size(playerWindow).height() - margin) {
+        return true;
+    }
 
-	// Check around widgets
-	for (int i = 0; i < widgets.size(); i++) {
-		QWidget* w = widgets[i];
-		QRect showArea(w->mapToGlobal(QPoint(0, 0)) - QPoint(margin, margin),
-					   w->size() + QSize(margin, margin));
-		if (showArea.contains(QCursor::pos())) {
-			return true;
-		}
-	}
+    // Check around widgets
+    for (int i = 0; i < widgets.size(); i++) {
+        QWidget* w = widgets[i];
+        QRect showArea(w->mapToGlobal(QPoint(0, 0)) - QPoint(margin, margin),
+                       w->size() + QSize(margin, margin));
+        if (showArea.contains(QCursor::pos())) {
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 void TAutoHideTimer::onActionToggled(bool visible) {
 
-	if (settingVisible)
-		return;
+    if (settingVisible)
+        return;
 
-	QString actioName = QObject::sender()->objectName();
-	TItemMap::const_iterator i = items.find(actioName);
-	if (i == items.end()) {
+    QString actioName = QObject::sender()->objectName();
+    TItemMap::const_iterator i = items.find(actioName);
+    if (i == items.end()) {
         logger()->warn("onActionToggled: action '" + actioName + "' not found");
-		return;
-	}
+        return;
+    }
 
-	TAutoHideItem item = i.value();
-	if (visible) {
-		actions.append(item.action);
-		widgets.append(item.widget);
-		if (autoHide && enabled) {
-			QTimer::start();
-		}
-	} else {
-		actions.removeOne(item.action);
-		widgets.removeOne(item.widget);
-	}
+    TAutoHideItem item = i.value();
+    if (visible) {
+        actions.append(item.action);
+        widgets.append(item.widget);
+        if (autoHide && enabled) {
+            QTimer::start();
+        }
+    } else {
+        actions.removeOne(item.action);
+        widgets.removeOne(item.widget);
+    }
 }
 
 void TAutoHideTimer::showHiddenMouse() {
 
-	if (mouseHidden) {
-		mouseHidden = false;
-		playerWindow->setCursor(QCursor(Qt::ArrowCursor));
-	}
+    if (mouseHidden) {
+        mouseHidden = false;
+        playerWindow->setCursor(QCursor(Qt::ArrowCursor));
+    }
 }
 
 void TAutoHideTimer::hideMouse() {
 
-	if (!mouseHidden) {
-		mouseHidden = true;
-		playerWindow->setCursor(QCursor(Qt::BlankCursor));
-	}
+    if (!mouseHidden) {
+        mouseHidden = true;
+        playerWindow->setCursor(QCursor(Qt::BlankCursor));
+    }
 }
 
 void TAutoHideTimer::setDraggingPlayerWindow(bool dragging) {
 
-	draggingPlayerWindow = dragging;
-	if (autoHide && enabled && draggingPlayerWindow && visibleWidget()) {
-		setVisible(false);
-	}
+    draggingPlayerWindow = dragging;
+    if (autoHide && enabled && draggingPlayerWindow && visibleWidget()) {
+        setVisible(false);
+    }
 }
 
 void TAutoHideTimer::onTimeOut() {
 
-	// Handle mouse
-	if (autoHideMouse) {
-		if ((QCursor::pos() - autoHideMouseLastPosition).manhattanLength()
+    // Handle mouse
+    if (autoHideMouse) {
+        if ((QCursor::pos() - autoHideMouseLastPosition).manhattanLength()
             > MOUSE_MOVED_TRESHOLD) {
-			showHiddenMouse();
-			QTimer::start();
-		} else if (enabled) {
-			hideMouse();
-		}
-	} else {
-		showHiddenMouse();
-	}
-	autoHideMouseLastPosition = QCursor::pos();
+            showHiddenMouse();
+            QTimer::start();
+        } else if (enabled) {
+            hideMouse();
+        }
+    } else {
+        showHiddenMouse();
+    }
+    autoHideMouseLastPosition = QCursor::pos();
 
     // Hide widgets when no mouse buttons down and mouse outside show area
-	if (autoHide && enabled && visibleWidget()) {
+    if (autoHide && enabled && visibleWidget()) {
         if (QApplication::mouseButtons() || mouseInsideShowArea()) {
-			QTimer::start();
-		} else {
-			setVisible(false);
-		}
-	}
+            QTimer::start();
+        } else {
+            setVisible(false);
+        }
+    }
 }
 
 bool TAutoHideTimer::eventFilter(QObject* obj, QEvent* event) {
 
-	bool button = event->type() == QEvent::MouseButtonPress
-				  || event->type() == QEvent::MouseButtonRelease;
+    bool button = event->type() == QEvent::MouseButtonPress
+                  || event->type() == QEvent::MouseButtonRelease;
     bool mouse = button;
 
     if (event->type() == QEvent::MouseMove
@@ -231,28 +231,28 @@ bool TAutoHideTimer::eventFilter(QObject* obj, QEvent* event) {
         mouse = true;
     }
 
-	// Handle mouse
+    // Handle mouse
     if (mouse) {
-		showHiddenMouse();
-		if (autoHideMouse && enabled) {
-			QTimer::start();
-		}
-		autoHideMouseLastPosition = QCursor::pos();
-	}
+        showHiddenMouse();
+        if (autoHideMouse && enabled) {
+            QTimer::start();
+        }
+        autoHideMouseLastPosition = QCursor::pos();
+    }
 
-	// Handle widgets
-	if (autoHide && enabled && mouse) {
-		// Don't show when left button still down, like when dragging
-		if ((QApplication::mouseButtons() & Qt::LeftButton) == 0
-			&& hiddenWidget()) {
+    // Handle widgets
+    if (autoHide && enabled && mouse) {
+        // Don't show when left button still down, like when dragging
+        if ((QApplication::mouseButtons() & Qt::LeftButton) == 0
+            && hiddenWidget()) {
             if (button || mouseInsideShowArea()) {
-				setVisible(true);
-				QTimer::start();
-			}
-		}
-	}
+                setVisible(true);
+                QTimer::start();
+            }
+        }
+    }
 
-	return QTimer::eventFilter(obj, event);
+    return QTimer::eventFilter(obj, event);
 }
 
 } //namespace Gui
--- src/gui/autohidetimer.h
+++ src/gui/autohidetimer.h
@@ -12,69 +12,69 @@ namespace Gui {
 
 class TAutoHideItem {
 public:
-	QAction* action;
-	QWidget* widget;
+    QAction* action;
+    QWidget* widget;
 
-	TAutoHideItem() : action(0), widget(0) {}
-	TAutoHideItem(QAction* a, QWidget* w) : action(a), widget(w) {}
-	virtual ~TAutoHideItem();
+    TAutoHideItem() : action(0), widget(0) {}
+    TAutoHideItem(QAction* a, QWidget* w) : action(a), widget(w) {}
+    virtual ~TAutoHideItem();
 };
 
 class TAutoHideTimer : public QTimer {
-	Q_OBJECT
+    Q_OBJECT
     DECLARE_QCLASS_LOGGER
 
 public:
-	explicit TAutoHideTimer(QObject *parent, QWidget* playerwin);
-	virtual ~TAutoHideTimer();
+    explicit TAutoHideTimer(QObject *parent, QWidget* playerwin);
+    virtual ~TAutoHideTimer();
 
-	void add(QAction* action, QWidget* w);
+    void add(QAction* action, QWidget* w);
 
 public slots:
-	void start();
-	void stop();
+    void start();
+    void stop();
 
-	void enable();
-	void disable();
+    void enable();
+    void disable();
 
-	void setAutoHideMouse(bool on);
-	void startAutoHideMouse() { setAutoHideMouse(true); }
-	void stopAutoHideMouse() { setAutoHideMouse(false); }
-	void setDraggingPlayerWindow(bool dragging);
+    void setAutoHideMouse(bool on);
+    void startAutoHideMouse() { setAutoHideMouse(true); }
+    void stopAutoHideMouse() { setAutoHideMouse(false); }
+    void setDraggingPlayerWindow(bool dragging);
 
 protected:
-	bool eventFilter(QObject* obj, QEvent* event);
+    bool eventFilter(QObject* obj, QEvent* event);
 
 private:
-	bool autoHide;
-	bool enabled;
-	bool settingVisible;
+    bool autoHide;
+    bool enabled;
+    bool settingVisible;
 
-	bool autoHideMouse;
-	bool mouseHidden;
-	QPoint autoHideMouseLastPosition;
-	bool draggingPlayerWindow;
+    bool autoHideMouse;
+    bool mouseHidden;
+    QPoint autoHideMouseLastPosition;
+    bool draggingPlayerWindow;
 
-	QWidget* playerWindow;
+    QWidget* playerWindow;
 
-	QList<QAction*> actions;
-	QList<QWidget*> widgets;
+    QList<QAction*> actions;
+    QList<QWidget*> widgets;
 
-	typedef QMap<QString, TAutoHideItem> TItemMap;
-	TItemMap items;
+    typedef QMap<QString, TAutoHideItem> TItemMap;
+    TItemMap items;
 
-	bool hiddenWidget() const;
-	bool visibleWidget() const;
+    bool hiddenWidget() const;
+    bool visibleWidget() const;
 
-	bool mouseInsideShowArea() const;
-	void setVisible(bool visible);
+    bool mouseInsideShowArea() const;
+    void setVisible(bool visible);
 
-	void showHiddenMouse();
-	void hideMouse();
+    void showHiddenMouse();
+    void hideMouse();
 
 private slots:
-	void onActionToggled(bool visible);
-	void onTimeOut();
+    void onActionToggled(bool visible);
+    void onTimeOut();
 };
 
 } // namespace Gui
