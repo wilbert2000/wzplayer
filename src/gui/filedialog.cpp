a2a7a7f4f5174a2dcbba2f9dccbe8c6c0ec6d9ff
wilbert2000 <wplayer@xs4all.nl>
2019-03-19 14:09:47 +0100

    Formatting

--- src/gui/filedialog.cpp
+++ src/gui/filedialog.cpp
@@ -22,40 +22,44 @@
 
 namespace Gui {
 
-QString TFileDialog::getOpenFileName(QWidget* parent,
-            const QString & caption, 
-            const QString & dir, const QString & filter, 
-            QString* selectedFilter, QFileDialog::Options options) 
+QString TFileDialog::getOpenFileName(
+        QWidget* parent,
+        const QString & caption,
+        const QString & dir, const QString & filter,
+        QString* selectedFilter, QFileDialog::Options options)
 {
     return QFileDialog::getOpenFileName(parent, caption, dir, filter,
                                          selectedFilter, options);
 }
 
-QString TFileDialog::getExistingDirectory (QWidget* parent,
-            const QString & caption, 
-            const QString & dir, 
-            QFileDialog::Options options)
+QString TFileDialog::getExistingDirectory(
+        QWidget* parent,
+        const QString & caption,
+        const QString & dir,
+        QFileDialog::Options options)
 {
     return QFileDialog::getExistingDirectory(parent, caption, dir, options);
 }
 
-QString TFileDialog::getSaveFileName (QWidget* parent,
-            const QString & caption, 
-            const QString & dir, 
-            const QString & filter, 
-            QString* selectedFilter, 
-            QFileDialog::Options options)
+QString TFileDialog::getSaveFileName(
+        QWidget* parent,
+        const QString & caption,
+        const QString & dir,
+        const QString & filter,
+        QString* selectedFilter,
+        QFileDialog::Options options)
 {
     return QFileDialog::getSaveFileName(parent, caption, dir, filter,
                                         selectedFilter, options);
 }
 
-QStringList TFileDialog::getOpenFileNames (QWidget* parent,
-            const QString & caption, 
-            const QString & dir, 
-            const QString & filter, 
-            QString* selectedFilter, 
-            QFileDialog::Options options)
+QStringList TFileDialog::getOpenFileNames(
+        QWidget* parent,
+        const QString & caption,
+        const QString & dir,
+        const QString & filter,
+        QString* selectedFilter,
+        QFileDialog::Options options)
 {
     return QFileDialog::getOpenFileNames(parent, caption, dir, filter,
                                          selectedFilter, options);
--- src/gui/filedialog.h
+++ src/gui/filedialog.h
@@ -30,27 +30,31 @@ namespace Gui {
 class TFileDialog {
 
 public:
-    static QString getOpenFileName(QWidget* parent = 0, 
+    static QString getOpenFileName(
+            QWidget* parent = 0,
             const QString & caption = QString(), 
             const QString & dir = QString(), 
             const QString & filter = QString(), 
             QString* selectedFilter = 0, 
             QFileDialog::Options options = QFileDialog::DontResolveSymlinks) ;
 
-    static QString getExistingDirectory (QWidget* parent = 0, 
+    static QString getExistingDirectory(
+            QWidget* parent = 0,
             const QString & caption = QString(), 
             const QString & dir = QString(), 
             QFileDialog::Options options = QFileDialog::ShowDirsOnly);
 
-    static QString getSaveFileName (QWidget* parent = 0, 
-            const QString & caption = QString(), 
-            const QString & dir = QString(), 
-            const QString & filter = QString(), 
-            QString* selectedFilter = 0, 
-            QFileDialog::Options options = QFileDialog::DontResolveSymlinks | 
+    static QString getSaveFileName(
+            QWidget* parent = 0,
+            const QString & caption = QString(),
+            const QString & dir = QString(),
+            const QString & filter = QString(),
+            QString* selectedFilter = 0,
+            QFileDialog::Options options = QFileDialog::DontResolveSymlinks |
                                            QFileDialog::DontConfirmOverwrite);
 
-    static QStringList getOpenFileNames (QWidget* parent = 0, 
+    static QStringList getOpenFileNames(
+            QWidget* parent = 0,
             const QString & caption = QString(), 
             const QString & dir = QString(), 
             const QString & filter = QString(), 
